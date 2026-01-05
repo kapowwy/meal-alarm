033b8c42fce9b23c659d1393a434c4884ce68cba
Proaw <pk488@exeter.ac.uk>
2026-01-05 21:27:11 +0700

    adjust center

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import MealAlarmApp from "./meal-alarm-app.jsx";
 
 export default function App() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-[#faf7f0] p-6">
-      <MealAlarmApp />
+    <div className="min-h-screen w-screen flex items-center justify-center bg-[#faf7f0] p-6">
+      <div className="w-full flex justify-center">
+        <MealAlarmApp />
+      </div>
     </div>
   );
 }
